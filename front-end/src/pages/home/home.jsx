b7e88631dab57b3e4c112836e8a34dd7078d73da
GustavoMS <gustavomorais47.gm@gmail.com>
2022-06-18 17:25:41 -0300

    Adição menu lateral e referencia

--- front-end/src/pages/home/home.jsx
+++ front-end/src/pages/home/home.jsx
@@ -1,33 +1,72 @@
 import React from 'react';
+import {Routes,Route,Link} from "react-router-dom";
 import './home.css';
+import Logo from '../../assets/LogoTopo.svg';
 
 export default function Home() {
     const User = 'Gustavo';
+    
+    var WidthTela;
+    var HeightTela;
 
-    function LayoutTopo() {
-        const LarguraMenuTopo = document.getElementById('menuTopo').clientWidth;
-        const LarguraMenuIcon = document.getElementById('menuIcon').clientWidth;
-        const LarguraMenuUser = document.getElementById('menuUser').clientWidth;
-        const LogoMenu = document.getElementById('menuLogo');
+    /*CRIA A LOGO DO CABEÇALHO*/
+    function ObterLargura() {
+        var LarguraMenuTopo = document.getElementById('menuTopo').clientWidth;
+        var LarguraMenuIcon = document.getElementById('menuIcon').clientWidth;
+        var LarguraMenuUser = document.getElementById('menuUser').clientWidth;
 
-        const LarguraMenuLogo = LarguraMenuTopo - (LarguraMenuIcon + LarguraMenuUser);
-        LogoMenu.style.width = LarguraMenuLogo;
-
+        CalcularLarguraLogo(LarguraMenuTopo, LarguraMenuIcon, LarguraMenuUser);
     }
+    function CalcularLarguraLogo(largura1, largura2, largura3) {
+        var LarguraMenuLogo = largura1 - (largura2 + largura3);
+        CriaDivLogo(LarguraMenuLogo);
+    }
+    function CriaDivLogo(largura) {
+        var DivPai = document.getElementById('menuTopo').innerHTML;
+        var ClassDivmenuLogo = document.getElementsByClassName('MenuLogo');
+        DivPai = '<div class="MenuIcon" id="menuIcon"><label>☰</label></div>' + '<a href="/home"><div class="MenuLogo" id="menuLogo" style="height: 30px;"><img class="Logo" src="' + Logo + '"></div></a>' + '<div class="MenuUser" id="menuUser"><label>Olá Gustavo</label></div>';
+        ClassDivmenuLogo.clientWidth = largura + 'px';
+        document.getElementById('menuTopo').innerHTML = DivPai;
+    }
+    /*REDIMENCIONA A SECTION*/
+    function AjustaSection(){
+        var Section = document.getElementById('menu');
+        Section.style.height = HeightTela + 'px';
+    }
+    /*FAZ O CALCULO DA TELA*/
+    function CalculaTela(){
+        WidthTela = window.screen.width;
+        HeightTela = window.screen.height;
+    }
+
+    function Btn(){
+        console.log('sdf');
+    }
+
     React.useEffect(() => {
-        LayoutTopo();
+        CalculaTela();
+        AjustaSection();
+        ObterLargura();
     }, []);
     return (
-        <div className='MenuTopo' id='menuTopo'>
-            <div className='MenuIcon' id='menuIcon'>
-                <label>&#9776;</label>
+        <section className='section-01' id='menu'>
+            <div className='MenuLateral' id='menuLateral'>
+                <ul>
+                    <Link to='/home'><li>Home</li></Link>
+                    <Link to='/home'><li>Módulos</li></Link>
+                    <Link to='/home'><li>Customizar</li></Link>
+                    <Link to='/home'><li>Configuração</li></Link>
+                    <Link to='/home'><li>Sair</li></Link>
+                </ul>
             </div>
-            <div className='Logo' id='menuLogo'>
-                <h1>Personal System</h1>
+            <div className='MenuTopo' id='menuTopo'>
+                <div className='MenuIcon' id='menuIcon' onClick={Btn()}>
+                    <label id='BtnMenu'>&#9776;</label>
+                </div>
+                <div className='MenuUser' id='menuUser'>
+                    <label>Olá {User}</label>
+                </div>
             </div>
-            <div className='MenuUser' id='menuUser'>
-                <label>Olá {User}</label>
-            </div>
-        </div>
+        </section>
     );
 }
