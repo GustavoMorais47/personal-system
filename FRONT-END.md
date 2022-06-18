007c75b9fb70ccb5424e1855d80b1fbfcbf813f1
GustavoMS <gustavomorais47.gm@gmail.com>
2022-06-18 00:39:44 -0300

    1º versão do Menu Topo

--- FRONT-END.md
+++ FRONT-END.md
@@ -1,4 +1,8 @@
+### `npm install`
+Execute caso não tenha a pasta 'node_modules'
+
 ### `npx create-react-app front-end`
 Inicia o projeto react 'front-end'
 
 ### `npm install --save react-router-dom`
+Utilizado para criar rotas com o React
--- front-end/src/pages/home/home.jsx
+++ front-end/src/pages/home/home.jsx
@@ -1,9 +1,33 @@
-import React from "react";
+import React from 'react';
+import './home.css';
 
-export default function Home(){
-    return(
-        <div>
-            <h1>Home</h1>
+export default function Home() {
+    const User = 'Gustavo';
+
+    function LayoutTopo() {
+        const LarguraMenuTopo = document.getElementById('menuTopo').clientWidth;
+        const LarguraMenuIcon = document.getElementById('menuIcon').clientWidth;
+        const LarguraMenuUser = document.getElementById('menuUser').clientWidth;
+        const LogoMenu = document.getElementById('menuLogo');
+
+        const LarguraMenuLogo = LarguraMenuTopo - (LarguraMenuIcon + LarguraMenuUser);
+        LogoMenu.style.width = LarguraMenuLogo;
+
+    }
+    React.useEffect(() => {
+        LayoutTopo();
+    }, []);
+    return (
+        <div className='MenuTopo' id='menuTopo'>
+            <div className='MenuIcon' id='menuIcon'>
+                <label>&#9776;</label>
+            </div>
+            <div className='Logo' id='menuLogo'>
+                <h1>Personal System</h1>
+            </div>
+            <div className='MenuUser' id='menuUser'>
+                <label>Olá {User}</label>
+            </div>
         </div>
     );
 }
